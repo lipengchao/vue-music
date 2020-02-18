954535163ee58c9bb205624d348c7c2252465b6f
雪中无处寻 <1179817689@qq.com>
2020-02-18 14:43:31 +0800

    项目初始化完成

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    哈哈哈
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~common/stylus/variable'
+
+#app
+  color: $color-theme;
+</style>
