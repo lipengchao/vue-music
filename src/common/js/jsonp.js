6443c6540eac56136d8bbbb3916481406e80efd4
雪中无处寻 <1179817689@qq.com>
2020-02-18 19:19:45 +0800

    jsonp封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,30 @@
+/**
+ * @author 雪中无处寻
+ * @date 2020/2/18 19:00
+ * @desc jsonp封装
+ */
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  // resolve 成功，reject失败
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+// 将data拼接到url上
+function param (data) {
+  let url = ''
+  for (let k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${encodeURIComponent(value)}`
+  }
+  return url ? url.substr(1) : ''
+}
