c4abd9296945d9fb191806a114add4d748aef742
雪中无处寻 <1179817689@qq.com>
2020-02-21 14:52:32 +0800

    播放器页面基本交互动画完成

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,21 +1,153 @@
 <template>
     <div class="player" v-show="playlist.length">
-      <div class="normal-player" v-show="fullScreen">
-        播放器
-      </div>
-      <div class="mini-player" v-show="!fullScreen"></div>
+      <transition
+        name="normal"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @leave="leave"
+        @after-leave="afterLeave"
+      >
+        <div class="normal-player" v-show="fullScreen">
+          <div class="background">
+            <img width="100%" height="100%" :src="currentSong.image" alt="">
+          </div>
+          <div class="top">
+            <div class="back" @click="back">
+              <i class="icon-back"></i>
+            </div>
+            <h1 class="title" v-html="currentSong.name"></h1>
+            <h2 class="subtitle" v-html="currentSong.singer"></h2>
+          </div>
+          <div class="middle">
+            <div class="middle-l">
+              <div class="cd-wrapper" ref="cdWrapper">
+                <div class="cd">
+                  <img class="image" :src="currentSong.image" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="operators">
+              <div class="icon i-left">
+                <i class="icon-sequence"></i>
+              </div>
+              <div class="icon i-left">
+                <i class="icon-prev"></i>
+              </div>
+              <div class="icon i-center">
+                <i class="needsclick  icon-play"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon-next"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon icon-not-favorite"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+      <transition name="mini">
+        <div class="mini-player" v-show="!fullScreen" @click="open">
+          <div class="icon">
+            <div class="imgWrapper" ref="miniWrapper">
+              <img width="40" height="40" :src="currentSong.image" alt="">
+            </div>
+          </div>
+          <div class="text">
+            <h2 class="name" v-html="currentSong.name"></h2>
+            <p class="desc" v-html="currentSong.singer"></p>
+          </div>
+          <div class="control">
+            <i class="icon-mini"></i>
+          </div>
+          <div class="control">
+            <i class="icon-playlist"></i>
+          </div>
+        </div>
+      </transition>
     </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import { mapGetters, mapMutations } from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import { prefixStyle } from 'common/js/dom'
+  const transform = prefixStyle('transform')
   export default {
     name: 'player',
     computed: {
       ...mapGetters([
         'fullScreen',
-        'playlist'
+        'playlist',
+        'currentSong'
       ])
+    },
+    methods: {
+      back () {
+        this.setFullScreen(false)
+      },
+      open () {
+        this.setFullScreen(true)
+      },
+      // 动画函数
+      enter (el, done) {
+        const { x, y, scale } = this._getPosAndScale()
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0, 0, 0) scale(1.1})`
+          },
+          100: {
+            transform: `translate3d(0, 0, 0) scale(1})`
+          }
+        }
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter () {
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave (el, done) {
+        this.$refs.cdWrapper.style.transition = 'all 0.4s'
+        const { x, y, scale } = this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave () {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
+      },
+      // 获取初始位置和缩放尺寸
+      _getPosAndScale () {
+        const targetWidth = 40
+        const paddingLeft = 40
+        const paddingBottom = 30
+        const paddingTop = 80
+        const width = window.innerWidth * 0.8
+        const scale = targetWidth / width
+        const x = -((window.innerWidth / 2) - paddingLeft)
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+        return {
+          x,
+          y,
+          scale
+        }
+      },
+      ...mapMutations({
+        setFullScreen: 'SET_FULL_SCREEN'
+      })
     }
   }
 </script>
@@ -32,12 +164,155 @@
       left: 0
       right: 0
       background: $color-background
+      .background
+        position: absolute
+        z-index: -1
+        left: 0
+        top: 0
+        width: 100%
+        height: 100%
+        opacity: .6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 25px
+        .back
+          position: absolute
+          top: 0
+          left: 6px
+          .icon-back
+            display: block
+            z-index: 50;
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          margin: 0 auto
+          width: 76%
+          line-height: 40px
+          text-align: center
+          font-size: $font-size-large
+          color: $color-text
+          no-wrap()
+        .subtitle
+          line-height: 20px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-text
+      .middle
+        position: fixed
+        top: 80px
+        bottom: 170px
+        width: 100%
+        white-space: nowrap
+        font-size: 0
+        .middle-l
+          position: relative
+          display: inline-block
+          vertical-align: top
+          width: 100%
+          height: 0
+          padding-top: 80%
+          .cd-wrapper
+            position: absolute
+            left: 10%
+            top: 0
+            width: 80%
+            height: 100%
+            box-sizing: border-box
+            .cd
+              width: 100%
+              height: 100%
+              border-radius: 50%
+              img
+                position: absolute
+                left: 0
+                top: 0
+                width: 100%
+                height: 100%
+                box-sizing: border-box
+                border-radius: 50%
+                border: 10px solid rgba(255, 255, 255, 0.1)
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .operators
+          display: flex
+          align-items: center
+          .icon
+            flex: 1
+            color: $color-theme
+            i
+              font-size: 30px
+            &.i-left
+              text-align: right
+            &.i-center
+              padding: 0 20px
+              text-align: center
+              i
+                font-size: 40px
+      &.normal-enter-active, &.normal-leave-active
+        transition: all 0.4s
+        .top, .bottom
+          transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+      &.normal-enter, &.normal-leave-to
+        opacity: 0
+        .top
+          transform: translate3d(0, -100px, 0)
+        .bottom
+          transform: translate3d(0, 100px, 0)
     .mini-player
       position: fixed
+      display: flex
       z-index: 180
+      align-items: center
       left: 0
       bottom: 0
       width: 100%
       height: 60px
       background: $color-highlight-background
+      &.mini-enter-active, &.mini-leave-active
+        transition: all 0.4s
+      &.mini-enter, &.mini-leave-to
+        opacity: 0
+      .icon
+        flex: 0 0 40px
+        width: 40px
+        height: 40px
+        padding: 0 10px 0 20px
+        .imgWrapper
+          width: 100%
+          height: 100%
+          img
+            border-radius: 50%
+      .text
+        display: flex
+        flex: 1
+        flex-direction: column
+        justify-content: center
+        line-height: 20px
+        overflow: hidden
+        .name
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d
+      .control
+        flex: 0 0 30px
+        width: 30px
+        padding: 0 10px
+        .icon-play-mini, .icon-pause-mini, .icon-playlist
+          font-size: 30px
+          color: $color-theme-d
+        .icon-mini
+          font-size: 32px
+          position: absolute
+          left: 0
+          top: 0
 </style>
